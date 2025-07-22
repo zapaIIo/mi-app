2e7e8c28f37b4658e2738586e9c937ad644b3740
zapallo <zatapallo@gmail.com>
2025-07-22 19:34:15 -0400

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react"
 
 function App(){
+    const valores=[
+      {color:"red",alto:77},
+      {color:"olive",alto:177},
+      {color:"orange",alto:101}
+    ]
     const[altura,setAltura]=useState(109);
+    const aumentar=()=>{
+      (altura>=225)
+      ? setAltura(10)
+      : setAltura(altura+25)
+    }
     return (
         <>
-        
-            <div className="container"> 
-                <div className="barra roja animar">
+            <div className="container containerGrande"> 
+                {valores.map((valor,i)=>
+                <div key={i} className="barra animar">
                 style={{"--altura":`${altura}px`,
-                height:`${altura}px`,
-                transition:'height 1s ease-out'}}
+                  left:`${10+(i*1000)}px`,
+                  backgroundColor:valor.color,
+                  transition:'height 1s ease-out'
+                }}
+               onClick={aumentar}
                     
                 </div>
+              )}
             </div>
 
         </>
