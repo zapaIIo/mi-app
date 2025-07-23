27cd313092b95113ee7553f6677b3fc6d54baa47
zapallo <zatapallo@gmail.com>
2025-07-22 20:44:45 -0400

    correccion_2

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,28 @@ function App(){
       {color:"red",alto:77},
       {color:"olive",alto:177},
       {color:"orange",alto:101}
-    ]
+    ];
     const[altura,setAltura]=useState(109);
     const aumentar=()=>{
       elementoRef.current.classList.toggle('stop')
     }
     return (
-        <>
             <div className="container "> 
+              <div 
+              ref={elementoRef} 
+              className="barra roja animar"
+              onClick={aumentar}
+              style={{"--altura":`${altura}px`}}
+              >
+                
+                
                
-                <div ref={elementoRef} className="barra roja animar">
-                style={{"--altura":`${altura}px`
-                }}
-               onClick={aumentar}
                     
                 </div>
               
             </div>
 
-        </>
+
 
     )
 }
